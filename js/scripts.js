d0ffdd6ab8a14cb75166675a9899afdd57223c7c
Julio Muller <juliodolago@gmail.com>
2020-04-04 10:56:03 -0300

    Created methods to buttons and added some flags to show/hide elements

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,32 @@ new Vue({
 
   data() {
     return {
-      hello: 'Hello, world!',
+      gameStopped: true,
+      gameOver: false,
+      won: false,
+      logs: [],
     }
+  },
+
+  watch: {
+    gameOver(value) {
+      this.gameStopped = value
+    }
+  },
+
+  methods: {
+    start() {
+      this.gameStopped = false
+      this.gameOver = false
+    },
+    attack(especial = false) {
+      console.log(`"${especial ? 'especial ' : ''}attack" triggered`)
+    },
+    heal() {
+      console.log('"heal" triggered')
+    },
+    giveup() {
+      this.gameStopped = true
+    },
   },
 })
